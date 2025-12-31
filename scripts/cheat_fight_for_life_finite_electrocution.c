68f6d8c54b3962e45ecdfdc0f33e1b1b0e0a4571
jguffer <jguffer@proton.me>
2025-12-31 11:41:54 -0500

    Updated to poll electrocuting variable

--- scripts/cheat_fight_for_life_finite_electrocution.c
+++ scripts/cheat_fight_for_life_finite_electrocution.c
@@ -20,6 +20,12 @@
 #define FIGHT4LIFE_HEALTHC_ADDR        0x001D890B
 #define FIGHT4LIFE_HEALTH_VALUE        255
 
+// Found using a Pro Action Replay like script that finds addresses of always decreasing values.
+#define FIGHT4LIFE_VOLUME_ADDR   0x0016EBCC
+
+// Found counting bytes backwards from volume addr, per FYBMAIN.S
+#define FIGHT4LIFE_ELECTR_ADDR   0x0016EB37
+
 // NTSC
 #define FIGHT4LIFE_FRAMES_PER_SECOND   60
 
@@ -37,7 +43,7 @@ static const int32_t skMcFlag_Loaded = (1 << 0);
 static uint64_t lastUpdateFrameCount = 0;
 
 // Setting handles.
-static int sNumberSecondsSettingHandle = INVALID;
+static int sHealthSettingHandle = INVALID;
 
 static uint32_t on_sw_loaded(const int eventHandle, void* pEventData)
 {
@@ -58,10 +64,10 @@ static uint32_t on_emu_frame(const int eventHandle, void* pEventData)
    {
       // Get setting values.  Doing this once per frame, allows player
       // to modify setting during game session.
-      int numberSecondsSettingValue = INVALID;
-      bigpemu_get_setting_value(&numberSecondsSettingValue, sNumberSecondsSettingHandle );
+      int healthSettingValue = INVALID;
+      bigpemu_get_setting_value(&healthSettingValue, sHealthSettingHandle );
 
-      if (numberSecondsSettingValue >= 0)
+      if (healthSettingValue > 0)
       {
          // Just write to all 3 locations.  Unsure if all 3 need
          // to be written to.
@@ -84,36 +90,11 @@ static uint32_t on_emu_frame(const int eventHandle, void* pEventData)
          // that tally/decrement remaining life into total score
          // at the end of match/round.  Failure would be infinite
          // loop.)
-         //
-         // If knew name of variable(s) (if variable declared in same
-         // file that declares energy_p1) that holding electrocution
-         // state, could probably locate variable in memory by
-         // offset.  Could then read that variable and not set
-         // health variable when being electrocuted, preventing the
-         // infinite loop.  Alternatively, maybe can read joypad
-         // inputs and when Paused and Option button on joypad is
-         // held down, don't write to health variable.
 
-         // Only max out health every n seconds.
-         //
-         // Updating every frame, causes an infinite loop if player
-         // gets electrocuted.
-         // This means player will lose round if electrocuted, but
-         // better then having the game go into infinite loop.
-         //
-         // N seconds is a workaround to allow the electrocution to
-         // complete, so that infinite loop is avoided.
-         //
-         // Thought of possible updating every frame, unless player presses
-         // and holds <Option>+3 during electrocution, but this seemed easier
-         // for me and doesn't require player knowing about having to press
-         // and hold certain joybad buttons at certain time during gameplay.
-         const uint64_t newFrameCount = bigpemu_jag_get_frame_count();
-         const uint64_t numSeconds = (uint64_t) numberSecondsSettingValue;
-         if (newFrameCount > (lastUpdateFrameCount + (FIGHT4LIFE_FRAMES_PER_SECOND * numSeconds)))
+         // Only max out health, if not being electrocuted
+         uint8_t electrocuting = bigpemu_jag_read8(FIGHT4LIFE_ELECTR_ADDR);
+         if (0 == electrocuting)
          {
-            lastUpdateFrameCount = newFrameCount;
-
             bigpemu_jag_write8(FIGHT4LIFE_HEALTHA_ADDR, FIGHT4LIFE_HEALTH_VALUE);
             bigpemu_jag_write8(FIGHT4LIFE_HEALTHB_ADDR, FIGHT4LIFE_HEALTH_VALUE);
             bigpemu_jag_write8(FIGHT4LIFE_HEALTHC_ADDR, FIGHT4LIFE_HEALTH_VALUE);
@@ -131,7 +112,7 @@ void bigp_init()
 
    // Register cheat settings with BigPEmu GUI interface.
    const int catHandle = bigpemu_register_setting_category(pMod, "Fight For Life");
-   sNumberSecondsSettingHandle = bigpemu_register_setting_int_full(catHandle, "Refill Health Every N Seconds", FIGHT4LIFE_DEFAULT_NUM_SECONDS, INVALID, FIGHT4LIFE_MAX_NUM_SECONDS, 1);
+   sHealthSettingHandle = bigpemu_register_setting_bool(catHandle, "Infinite Health", 1);
 
    sOnLoadEvent = bigpemu_register_event_sw_loaded(pMod, on_sw_loaded);
    sOnEmuFrame = bigpemu_register_event_emu_thread_frame(pMod, on_emu_frame);
@@ -145,5 +126,5 @@ void bigp_shutdown()
    bigpemu_unregister_event(pMod, sOnEmuFrame);
    sOnEmuFrame = INVALID;
 
-   sNumberSecondsSettingHandle = INVALID;
+   sHealthSettingHandle = INVALID;
 }
